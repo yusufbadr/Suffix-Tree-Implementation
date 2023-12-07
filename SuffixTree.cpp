4adb21c4f5dd4f3584fe707e6710b228bb073d12
Yusuf Badr <yusufbadr@yahoo.com>
2023-12-07 18:57:40 +0200

    initial implementation

--- SuffixTree.cpp
+++ SuffixTree.cpp
@@ -1,39 +1,118 @@
-//
-// Created by Yusuf Badr on 02/12/2023.
-//
+// <cstdlib>, <cstdio>, <cstring>, <iostream>
 
+#include <cstring>
+
+class SuffixNode;
+
+struct Node{
+    SuffixNode *data;
+    Node *next;
+};
+
+class LinkedList{
+public:
+
+
+    Node *head;
+
+    void insert(SuffixNode suffixNode);
+    Node* getMatchingNode(const char* text);
+
+};
+
+class SuffixNode{
+public:
+    SuffixNode(int startIndx, int suffixIndx){
+        edgeStartIndex = startIndx;
+        suffixIndex = suffixIndx;
+    }
+
+    int edgeStartIndex;
+    int suffixIndex;
+
+    // Linked List
+    LinkedList *childrenHead;
+
+private:
+};
 
 class SuffixTree{
 public:
-    SuffixTree(const char* text);
+    SuffixTree(char* text);
     void Search(const char* text);
     ~SuffixTree();
 
+    SuffixNode *root;
 private:
 
-    class Node{
-    public:
-        Node(int startIndx, int suffixIndx){
-            edgeStartIndex = startIndx;
-            suffixIndex = suffixIndx;
-        }
-
-    private:
-        int edgeStartIndex;
-        int suffixIndex;
-
-        // Linked List
-        class ChildNode{
-            Node* data;
-            ChildNode* next;
-            ChildNode(Node* inData){
-                data = inData;
-                next = nullptr;
-            }
-        };
-        ChildNode* head;
-    };
 
 };
+
+SuffixTree::SuffixTree(char *text) {
+    root = new SuffixNode(-1, -1);
+
+    strcat(text, "$");
+    int len = (int) strlen(text);
+    for (int i = 0; i <len ; ++i) {
+        char *suffix = new char[len-i];
+        for (int j = i; j < len; ++j) {
+            suffix[j-i] = text[j];
+        }
+
+        // now i have a suffix
+        SuffixNode *currentSuffixNode = root;
+        while (currentSuffixNode != nullptr){
+            LinkedList *currentLinkedListNode = currentSuffixNode->childrenHead;
+
+            while(currentLinkedListNode != nullptr){
+                // if matching prefix
+
+
+                // len is absolute difference(edgeStart of children)
+
+                Node *childHead = currentLinkedListNode->head;
+                int minChildEdgeStart;
+                if (childHead!= nullptr){
+                    minChildEdgeStart = childHead->data->edgeStartIndex;
+                    childHead = childHead->next;
+                }
+
+                while (childHead!= nullptr){
+                    if(childHead->data->edgeStartIndex < minChildEdgeStart){
+                        minChildEdgeStart=childHead->data->edgeStartIndex;
+                    }
+                    childHead = childHead->next;
+                }
+
+                int lenSubstr = minChildEdgeStart - currentLinkedListNode->head->data->edgeStartIndex;
+
+
+                for (int j = currentLinkedListNode->head->data->edgeStartIndex; j < lenSubstr; ++j) {
+                    if (text[]);
+                }
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+        delete[] suffix;
+    }
+
+
+
+}
+
+void SuffixTree::Search(const char *text) {
+
+}
 
 
