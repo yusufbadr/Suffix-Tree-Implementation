618dee4d9feb5311d3bc973c45f7367c0e377955
Yusuf Badr <yusufbadr@yahoo.com>
2023-12-13 10:41:03 +0200

    endl to '\n'

--- SuffixTree2.cpp
+++ SuffixTree2.cpp
@@ -248,7 +248,7 @@ public:
     void Search(char *string){
         stringQuery = string;
         searchUtil(0, root->data->children.head);
-        std::cout << std::endl;
+        std::cout << '\n';
     }
 
     void printUtil(Node *root){
@@ -284,99 +284,101 @@ public:
 
 int main()
 {
-//    SuffixTree t("bananabanaba$");
-//    t.Search("ana"); // Prints: 1 3 7
-//    t.Search("naba"); // Prints: 4 8
-//    t.Search("banana0"); // NOT FOUND
-//    std::cout << std::endl;
-//
-//
-//
-//    SuffixTree t1("programming$");
-//    t1.Search("gram");  // Prints: 3
-//    t1.Search("pro");   // Prints: 0
-//    t1.Search("ming");  // Prints: 7
-//    std::cout << std::endl;
-//
-//
-//
-//    // Case 2
-//    SuffixTree t2("abracadabra$");
-//    t2.Search("bra");   // Prints: 1 8
-//    t2.Search("ada");   // Prints: 5
-//    t2.Search("abr");   // Prints: 0 7
-//    std::cout << std::endl;
-//
-//
-//    // Case 3
-//    SuffixTree t3("mississippi$");
-//    t3.Search("ssi");   // Prints: 2 5
-//    t3.Search("iss");   // Prints: 1 4
-//    t3.Search("ssippi");// Prints: 5
-//    std::cout << std::endl;
-//
-//
-//
-//    // Case 4
-//    SuffixTree t4("algorithmabcbacacca$");
-//    t4.Search("rithm"); // Prints: 4
-//    t4.Search("algo");  // Prints: 0
-//    t4.Search("ca"); // 14 17
-//    std::cout << std::endl;
-//
-//    // Case 5
-//    SuffixTree t5("datastructures$");
-//    t5.Search("data"); // Prints: 0
-//    t5.Search("a");  // Prints: 1 3
-//    t5.Search("xyz");
-//    std::cout << std::endl;
-//
-//
-//    // Case 6
-//    SuffixTree t6("abcdefgh$");
-//    t6.Search("def");   // Prints: 3
-//    t6.Search("gh");    // Prints: 6
-//    t6.Search("abc");   // Prints: 0
-//    std::cout << std::endl;
-//
-//    // Case 7
-//    SuffixTree t7("xyxxy$");
-//    t7.Search("yx");    // Prints: 1
-//    t7.Search("xx");    // Prints: 2
-//    t7.Search("x"); // 0 2 3
-//    std::cout << std::endl;
-//
-//    // Case 8
-//    SuffixTree t8("example$");
-//    t8.Search("mpl");   // Prints: 4
-//    t8.Search("exam");  // Prints: 0
-//    t8.Search("ple");   // Prints: 6
-//    std::cout << std::endl;
-//
-//    // Case 9
-//    SuffixTree t9("testing$");
-//    t9.Search("ing");   // Prints: 5
-//    t9.Search("test");  // Prints: 0
-//    t9.Search("ting");  // Prints: 6
-//    std::cout << std::endl;
-//
-//    // Case 10
-//    SuffixTree t10("abcdefgabcdefgabcdefg$");
-//    t10.Search("cd");   // Prints: 2
-//    t10.Search("ab");   // Prints: 0
-//    t10.Search("fg");   // Prints: 5
-//    std::cout << std::endl;
+    SuffixTree t("bananabanaba$");
+    t.Search("ana"); // Prints: 1 3 7
+    t.Search("naba"); // Prints: 4 8
+    t.Search("banana0"); // NOT FOUND
+    std::cout << '\n';
 
 
-    SuffixTree t1("abracadabraabrakadabraabracadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabra$");
+    SuffixTree t1("programming$");
+    t1.Search("gram");  // Prints: 3
+    t1.Search("pro");   // Prints: 0
+    t1.Search("ming");  // Prints: 7
+    std::cout << '\n';
 
-    // Test Case 4
-    t1.Search("brak"); // Prints: 8 33 58 83
-    std::cout << std::endl;
 
-    // Test Case 7
-    t1.Search("abrakadabra"); // Prints: 11 36 61
-    std::cout << std::endl;
+    // Case 2
+    SuffixTree t2("abracadabra$");
+    t2.Search("bra");   // Prints: 1 8
+    t2.Search("ada");   // Prints: 5
+    t2.Search("abr");   // Prints: 0 7
+    std::cout << '\n';
+
+
+    // Case 3
+    SuffixTree t3("mississippi$");
+    t3.Search("ssi");   // Prints: 2 5
+    t3.Search("iss");   // Prints: 1 4
+    t3.Search("ssippi");// Prints: 5
+    std::cout << '\n';
+
+
+    // Case 4
+    SuffixTree t4("algorithmabcbacacca$");
+    t4.Search("rithm"); // Prints: 4
+    t4.Search("algo");  // Prints: 0
+    t4.Search("ca"); // 14 17
+    std::cout << '\n';
+
+
+    // Case 5
+    SuffixTree t5("datastructures$");
+    t5.Search("data"); // Prints: 0
+    t5.Search("a");  // Prints: 1 3
+    t5.Search("xyz");
+    std::cout << '\n';
+
+
+    // Case 6
+    SuffixTree t6("abcdefgh$");
+    t6.Search("def");   // Prints: 3
+    t6.Search("gh");    // Prints: 6
+    t6.Search("abc");   // Prints: 0
+    std::cout << '\n';
+
+
+    // Case 7
+    SuffixTree t7("xyxxy$");
+    t7.Search("yx");    // Prints: 1
+    t7.Search("xx");    // Prints: 2
+    t7.Search("x"); // 0 2 3
+    std::cout << '\n';
+
+
+    // Case 8
+    SuffixTree t8("example$");
+    t8.Search("mpl");   // Prints: 4
+    t8.Search("exam");  // Prints: 0
+    t8.Search("ple");   // Prints: 6
+    std::cout << '\n';
+
+
+    // Case 9
+    SuffixTree t9("testing$");
+    t9.Search("ing");   // Prints: 5
+    t9.Search("test");  // Prints: 0
+    t9.Search("ting");  // Prints: 6
+    std::cout << '\n';
+
+
+    // Case 10
+    SuffixTree t10("abcdefgabcdefgabcdefg$");
+    t10.Search("cd");   // Prints: 2
+    t10.Search("ab");   // Prints: 0
+    t10.Search("fg");   // Prints: 5
+    std::cout << '\n';
+
+
+//    SuffixTree t1("abracadabraabrakadabraabracadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabraabrakadabra$");
+//
+//    // Test Case 4
+//    t1.Search("brak"); // Prints: 8 33 58 83
+//    std::cout << std::endl;
+//
+//    // Test Case 7
+//    t1.Search("abrakadabra"); // Prints: 11 36 61
+//    std::cout << std::endl;
 
 
     return 0;
